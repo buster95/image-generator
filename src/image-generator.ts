6adfce958ee9e9af18e9f771047b25f5cf870f32
Walter Corrales <corraleswalter@live.com>
2022-07-30 07:38:55 -0600

    [📦] remove package html2canvas

--- src/image-generator.ts
+++ src/image-generator.ts
@@ -1,12 +1,11 @@
 import path from 'path';
 import playwright from 'playwright';
-import html2canvas from 'html2canvas';
 
 export async function generate() {
     const browser = await playwright.chromium.launch();
     const page = await browser.newPage({ viewport: { width: 600, height: 338 } });
     await page.goto(`file://${path.join(__dirname, 'badge-template/index.html')}`);
-    await page.evaluate(() => { })
+    await page.evaluate(() => { });
     const image = await page.screenshot({ path: 'screenshot.png' });
     await browser.close();
     return image;
