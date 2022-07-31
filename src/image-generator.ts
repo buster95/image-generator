dbc38dd0d6bbfb8acdc36c443962f75dce96ff04
Walter Corrales <corraleswalter@live.com>
2022-07-30 18:01:00 -0600

    [✨] image changing labels dynamically

--- src/image-generator.ts
+++ src/image-generator.ts
@@ -1,11 +1,24 @@
 import path from 'path';
 import playwright from 'playwright';
 
-export async function generate() {
+type ImageData = {
+    username: string;
+    category: string;
+};
+
+export async function generate(data?: ImageData): Promise<Buffer> {
     const browser = await playwright.chromium.launch();
     const page = await browser.newPage({ viewport: { width: 600, height: 338 } });
     await page.goto(`file://${path.join(__dirname, 'badge-template/index.html')}`);
-    await page.evaluate(() => { });
+    if (data) {
+        page.evaluate((args) => {
+            const { username, category } = args;
+            const spanUsername = document.querySelector('#username');
+            const spanCategory = document.querySelector('#category');
+            if (spanUsername) spanUsername.textContent = username;
+            if (spanCategory) spanCategory.textContent = category;
+        }, data);
+    };
     const image = await page.screenshot({ path: 'screenshot.png' });
     await browser.close();
     return image;
--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// This line is not important, is just to show template in / route
+app.use(express.static(path.join(__dirname, './badge-template')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'image-template.html'));
+    res.sendFile(path.join(__dirname, './badge-template/index.html'));
 });
 
 app.get('/generate', async (req, res) => {
+    const { username, category } = req.query;
     res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(Buffer.from(await generate()));
+    res.end(Buffer.from(await generate({ username: (username as string) || 'default', category: (category as string) || 'default category' })));
 });
 
 app.listen(3000, () => {
